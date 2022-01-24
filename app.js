6b3b282022b4cf2d4f1ccb93850fc6a699db802b
DaehyeonKim <Daehyeonoff@gmail.com>
2022-01-24 13:01:42 +0900

    20220124

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
+"use strict";
+
+//모듈
 const express = require("express");
-const res = require("express/lib/response");
 const app = express();
+
+//라우팅
+const home = require("./routes/home");
+//포트 
+const POST = 3000;
 
 //앱세팅
 app.set("views", "./views");
 app.set("view engine", "ejs");
 
-app.get('/', (req, res) => {
- res.send(
- );   
-});
 
-app.get('/login', (req, res) => {
-    res.send(`
 
-`);
-});
+app.use("/", home); //use --> 미들 웨어 등록;
 
-app.listen(3000, () => {
+app.listen(POST, () => {
  console.log("서버 가동");
 }); 
