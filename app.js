ee1b1ffade528b8ad7051927d6a9de3cbe56f464
DaehyeonKim <Daehyeonoff@gmail.com>
2022-01-24 11:53:18 +0900

    20220124

--- app.js
+++ app.js
@@ -1,6 +1,32 @@
-const express = require("express");
-const app = express();
-
-app.listen(3000, () => {
-    console.log("서버 콘솔");
+const http = require("http");
+const app = http.createServer((req, res) => {
+    if(req.url === '/') {
+        res.end("여기는 루트입니다")
+    } else if (req.url ==='/login') {
+        res.end("여기는 로그인 화면입니다");
+    }
 });
+
+
+app.listen(3001, () => {
+    console.log("http로 가동된 서버.");
+});
+
+
+
+
+// const express = require("express");
+// const res = require("express/lib/response");
+// const app = express();
+
+// app.get('/', (req, res) => {
+//  res.send("여기는 루트입니다");   
+// });
+
+// app.get('/login', (req, res) => {
+//     res.send("여기는 로그인");
+// });
+
+// app.listen(3000, () => {
+//  console.log("서버 가동");
+// });
