edccd31ff974596e9f72d7edf60bf747ae9fcf20
DaehyeonKim <Daehyeonoff@gmail.com>
2022-01-24 12:51:14 +0900

    20220124

--- app.js
+++ app.js
@@ -2,39 +2,18 @@ const express = require("express");
 const res = require("express/lib/response");
 const app = express();
 
+//앱세팅
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
 app.get('/', (req, res) => {
- res.send(`
- <!DOCTYPE html>
- <html lang="ko">
- <head>
-     <meta charset="UTF-8">
-     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <title>Document</title>
- </head>
- <body>
-     여기는 루트입니다.
- </body>
- </html> 
- `);   
+ res.send(
+ );   
 });
 
 app.get('/login', (req, res) => {
     res.send(`
-<!DOCTYPE html>
-<html lang="ko">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <input type="text" placeholder="아이디"><br>
-    <input type="text" placeholder="비밀번호"><br>
-    <button>로그인</button>
-</body>
-</html> 
+
 `);
 });
 
