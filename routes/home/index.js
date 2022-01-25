af12a5b517e714cfa2bd864477fab14d72e62d0b
DaehyeonKim <Daehyeonoff@gmail.com>
2022-01-25 21:38:11 +0900

    20220125

--- routes/home/index.js
+++ routes/home/index.js
@@ -2,11 +2,11 @@
 const express = require("express");
 const router = express.Router();
 
-router.get('/', hello);
+const ctrl = require("./home.ctrl");
 
-router.get('/login', (req, res) => {
-    res.render("home/login");
-});
+router.get('/', ctrl.hello);
+
+router.get('/login', ctrl.login);
 
 
 module.exports = router;
