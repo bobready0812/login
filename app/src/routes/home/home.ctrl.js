47075306a49b8ba17a84bc5e47867f53c3d7bc3b
DaehyeonKim <Daehyeonoff@gmail.com>
2022-01-26 17:32:20 +0900

    20220126

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,14 +1,51 @@
 "use strict";
 
-const home = (req, res) => {
+const { use } = require(".");
+
+
+
+const output = {
+home : (req, res) => {
     res.render("home/index");
+},
+
+login : (req, res) => {
+    res.render("home/login");
+},
+
 };
 
-const login = (req, res) => {
-    res.render("home/login");
+
+const user = {
+    id: ["1", "2", "3"],
+    psword: ["1", "2", "3"]
+}; 
+
+const process = {
+    login:(req, res) => {
+       const id = req.body.id;
+       const psword = req.body.psword;
+       if (user.id.includes(id)) {
+           const idx = user.id.indexOf(id);
+           if(user.psword[idx] === psword) {
+               return res.json(
+                   {
+                       success: true,
+                   }
+               );
+           }
+       }
+
+       return res.json({
+           success:false,
+           msg:"로그인 실패",
+       });
+    },
 };
+
+
 
 module.exports = {
-    home,
-    login,
+ process,
+ output,
 }
