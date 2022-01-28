3ac83b709a2d3b132d52e4c51a057059fa59f532
DaehyeonKim <Daehyeonoff@gmail.com>
2022-01-28 21:25:55 +0900

    20220128

---
+++ app/src/model/UserStorage.js
@@ -0,0 +1,12 @@
+"use strict"
+
+
+class UserStorage {
+    user = {
+        id: ["1", "2", "3"],
+        psword: ["1", "2", "3"]
+    }; 
+}
+
+
+module.exports = UserStorage;
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,7 +1,6 @@
 "use strict";
 
-const { use } = require(".");
-
+const UserStorage = require("../../model/UserStorage");
 
 
 const output = {
@@ -16,32 +15,28 @@ login : (req, res) => {
 };
 
 
-const user = {
-    id: ["1", "2", "3"],
-    psword: ["1", "2", "3"]
-}; 
+
 
 const process = {
     login:(req, res) => {
        const id = req.body.id;
        const psword = req.body.psword;
-       if (user.id.includes(id)) {
-           const idx = user.id.indexOf(id);
-           if(user.psword[idx] === psword) {
-               return res.json(
-                   {
-                       success: true,
-                   }
-               );
-           }
-       }
-
-       return res.json({
-           success:false,
-           msg:"로그인 실패",
-       });
-    },
-};
+        
+       const userStorage = new UserStorage();
+       console.log(userStorage.user);
+//        const response = {};
+//        if (user.id.includes(id)) {
+//            const idx = user.id.indexOf(id);
+//            if(user.psword[idx] === psword) {
+//                response.success = true;
+//                return res.json(response);
+//            }
+//        }
+//        response.success = false;
+//        response.msg = "로그인 실패";
+//        return res.json(response);
+//     },
+// };
 
 
 
