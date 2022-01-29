7d39b6958853ad722cfa6cc59babf7180599dc07
DaehyeonKim <Daehyeonoff@gmail.com>
2022-01-29 15:51:05 +0900

    20220129

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -19,7 +19,7 @@ const process = {
     login:(req, res) => {
       const user = new User(req.body);
       const response = user.login();
-      console.log(response);
+      return res.json(response);
     },
 };
 
