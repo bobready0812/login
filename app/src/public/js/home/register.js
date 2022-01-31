c5defdbf1228a685101efebc1e1d34623338b63d
DaehyeonKim <Daehyeonoff@gmail.com>
2022-01-31 20:57:55 +0900

    20220131

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -2,32 +2,38 @@
 
 const id = document.querySelector("#id");
 const psword = document.querySelector("#psword");
-const loginBtn = document.querySelector("button");
-console.log("register");
-loginBtn.addEventListener("click", login);
+const name = document.querySelector("#name");
+const confirmPsword = document.querySelector("#confirm-psword");
+const registerBtn = document.querySelector("button");
 
-function login () {
+registerBtn.addEventListener("click", register);
+
+function register () {
+   
     const req = {
         id: id.value,
         psword: psword.value,
+        name: name.value,
+        confirmPsword: confirmPsword.value,
     };
+    console.log(req);
 
-    fetch("/login", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(req),
-    })
-    .then((res) => res.json())
-    .then((res) => {
-        if(res.success){
-            location.href= "/";
-        } else {
-            alert(res.msg);
-        }
-    })
-    .catch((err) => {
-        console.error(new Error("로그인 중 에러 발생"));
-    });
+    // fetch("/register", {
+    //     method: "POST",
+    //     headers: {
+    //         "Content-Type": "application/json",
+    //     },
+    //     body: JSON.stringify(req),
+    // })
+    // .then((res) => res.json())
+    // .then((res) => {
+    //     if(res.success){
+    //         location.href= "/";
+    //     } else {
+    //         alert(res.msg);
+    //     }
+    // })
+    // .catch((err) => {
+    //     console.error(new Error("로그인 중 에러 발생"));
+    // });
 }
