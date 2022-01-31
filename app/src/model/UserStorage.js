f0285ebafe1cc15c05c66c4e03ad76dff189bd08
DaehyeonKim <Daehyeonoff@gmail.com>
2022-02-01 01:03:53 +0900

    20220201

--- app/src/model/UserStorage.js
+++ app/src/model/UserStorage.js
@@ -1,16 +1,14 @@
 "use strict"
 
 
+const fs = require("fs");
+
 class UserStorage {
-   static #user = {
-        id: ["1", "2", "3"],
-        psword: ["1", "2", "3"],
-        name:["1", "2", "3"],
-    }; 
+  
 
 
     static getUser(...fields) {
-       const users = this.#user;
+    //    const users = this.#user;
        const newUsers = fields.reduce((newUsers, field) => {
            if(users.hasOwnProperty(field)) {
                newUsers[field] = users[field];
@@ -22,19 +20,23 @@ class UserStorage {
     }
 
     static getUserInfo(id) {
-   const users = this.#user;
-   const idx = users.id.indexOf(id);
-   const userInfo = Object.keys(users).reduce((newUser, info) => {
-       newUser[info] = users[info][idx];
-       return newUser;
-   }, {});
+//    const users = this.#user;
+fs.readFile("./package.json", (err,data) => {
+    if(err) throw err;
+    console.log(data);
+})
+//    const idx = users.id.indexOf(id);
+//    const userInfo = Object.keys(users).reduce((newUser, info) => {
+//        newUser[info] = users[info][idx];
+//        return newUser;
+//    }, {});
 
 
-   return userInfo;
+//    return userInfo;
     }
 
    static save(userInfo) {
-     const users = this.#user;
+    //  const users = this.#user;
      users.id.push(userInfo.id);
      users.name.push(userInfo.name);
      users.psword.push(userInfo.psword);
--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -9,12 +9,16 @@ const registerBtn = document.querySelector("#button");
 registerBtn.addEventListener("click", register);
 
 function register () {
+    if(!id.value) return alert("아이디 입력해주세요");
+    if(psword !== confirmPsword) {
+        return alert("비밀번호가 일치하지 않습니다.")
+    };
    
     const req = {
         id: id.value,
         psword: psword.value,
         name: name.value,
-        confirmPsword: confirmPsword.value,
+        
     };
     
     fetch("/register", {
