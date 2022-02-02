c14831c5460c8ca815985bbf09eb8516d2e534ab
DaehyeonKim <Daehyeonoff@gmail.com>
2022-02-02 14:16:56 +0900

    20220202

--- app/src/model/UserStorage.js
+++ app/src/model/UserStorage.js
@@ -57,7 +57,7 @@ class UserStorage {
 static async save(userInfo) {
     const users = await this.getUser(true);
     if(users.id.includes(userInfo.id)) {
-      return new Error("이미 존재하는 아이디입니다");
+      throw "이미 존재하는 아이디입니다";
     }
     users.id.push(userInfo.id);
     users.name.push(userInfo.name);
